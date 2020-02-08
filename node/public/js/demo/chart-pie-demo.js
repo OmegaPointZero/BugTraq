577f0b43739471f93329c91756777e5bbb688d8b
OmegaPointZero <joshua.erik.campbell@gmail.com>
2020-02-08 01:08:17 -0700

    dashboard 90% polished, looks good, almost ready for presentation, README contains more dev notes

--- node/public/js/demo/chart-pie-demo.js
+++ node/public/js/demo/chart-pie-demo.js
@@ -5,10 +5,15 @@ Chart.defaults.global.defaultFontColor = '#858796';
 // Pie Chart Example
 var ctx1 = document.getElementById("resolvedPieChart");
 var ctx2 = document.getElementById("openPieChart");
+var ctx3 = document.getElementById("totalPieChart");
 var data = JSON.parse(document.getElementById("resolvedBugTracker").textContent);
 var data2 = JSON.parse(document.getElementById("openBugTracker").textContent);
 var d1 = [data["Critical"], data["High"], data["Medium"], data["Low"], data["Documentation"], data["Feature Request"]]
 var d2 = [data2["Critical"], data2["High"], data2["Medium"], data2["Low"], data2["Documentation"], data2["Feature Request"]]
+var d3 = d1;
+for(var i=0;i<d3.length;i++){
+    d3[i] = d3[i] + d2[i]
+}
 
 var resolvedPieChart = new Chart(ctx1, {
   type: 'doughnut',
@@ -46,6 +51,36 @@ var openPieChart = new Chart(ctx2, {
     labels: ["Critical", "High", "Medium", "Low", "Documentation", "Feature Request"],
     datasets: [{
       data: d2,
+      backgroundColor: ['#e74a3b', '#e0893c', '#f6c23e',  '#1cc88a', '#4e73df', '#36b9cc'],
+      hoverBackgroundColor: ['#cc4227', '#da6e27', '#e2af26', '#17a673', '#2e59d9',  '#2c9faf'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
+
+var totalPieChart = new Chart(ctx3, {
+  type: 'doughnut',
+  data: {
+    labels: ["Critical", "High", "Medium", "Low", "Documentation", "Feature Request"],
+    datasets: [{
+      data: d3,
       backgroundColor: ['#e74a3b', '#e0893c', '#f6c23e',  '#1cc88a', '#4e73df', '#36b9cc'],
       hoverBackgroundColor: ['#cc4227', '#da6e27', '#e2af26', '#17a673', '#2e59d9',  '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
